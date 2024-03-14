c7a727bc7785dd74252f1801e59a49c78f170a40
nandishbt <nandishbt2001@gmail.com>
2024-03-14 17:31:23 +0530

    completed

--- src/Pages/RealEstate/RealEstate.jsx
+++ src/Pages/RealEstate/RealEstate.jsx
@@ -21,10 +21,10 @@ const RealEstate = () => {
       <p className='pp'>	Buying or selling a property? Look no further. Dream Home Associates specializes in real estate transactions.</p>
       <p className='pp'>	We assist you in finding your ideal home, evaluating properties, negotiating deals, and handling legal formalities.</p>
       <h1>TYPES OF SERVICES WE PROVIDE</h1>
-      <div className='container display-flex'>
+      <div className=' display-flex '>
       <Service_card heading="Real Estate " subtitle="Residential Flats, Villas, Plots, and Communities in both affordable and premium ranges" icon={<BsHouses />}/>
       <Service_card heading="Commercial" subtitle="Commercial Property Solutions" icon={<BsHouses />}/>
-      <Service_card heading="Interior Design " subtitle="Whether it’s a cozy apartment or a sprawling villa or fabulous Office spaces our experts curate designs that resonate with you." icon={<BsHouses />}/>
+      <Service_card heading="Interior Design " subtitle="Whether it’s a cozy apartment or fabulous Office spaces our experts curate designs that resonate with you." icon={<BsHouses />}/>
       <Service_card heading="Vastu " subtitle="Harmony starts at Home with Vastu. Our Vastu Experts & Architects design the house where Vastu science meets spirituality" icon={<BsHouses />}/>
 
 
