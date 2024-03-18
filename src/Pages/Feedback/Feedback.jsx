57c469d7ee784a42fd3589367c8e946b660819f6
nandishbt <nandishbt2001@gmail.com>
2024-03-18 10:01:37 +0530

    c

--- src/Pages/Feedback/Feedback.jsx
+++ src/Pages/Feedback/Feedback.jsx
@@ -8,7 +8,10 @@ import Testimos from '../../Components/Testimos/Testimos'
 const Feedback = () => {
   return (
 
+    
+
     <>
+    
     <Navbar />
     <div className='MainFeed'>
       <div className='Innerdiv'>
@@ -20,14 +23,27 @@ const Feedback = () => {
         <input text='name' name='name' placeholder='Enter Your name' required /><br></br>
         <label>YOUR EMAIL:</label><br></br>
         <input text='email' name='email' placeholder='Enter Your email' required /><br></br>
-        <label>RATING(1-5) (1 being the low & 5 being the high)</label><br></br>
-        <select name="dropdown"  className='dropdn' required>
+        <label>RATE US:</label><br></br>
+        {/* <select name="dropdown"  className='dropdn' required>
             <option value="option1" selected>1</option>
             <option value="option2">2</option>
             <option value="option3">3</option>
             <option value="option2">4</option>
             <option value="option3">5</option>
-        </select><br></br>
+        </select><br></br> */}
+
+<div class="star-rating">
+    <input type="radio" id="star5" name="rating" value="5" />
+    <label for="star5" title="5 stars">★</label>
+    <input type="radio" id="star4" name="rating" value="4" />
+    <label for="star4" title="4 stars">★</label>
+    <input type="radio" id="star3" name="rating" value="3" />
+    <label for="star3" title="3 stars">★</label>
+    <input type="radio" id="star2" name="rating" value="2" />
+    <label for="star2" title="2 stars">★</label>
+    <input type="radio" id="star1" name="rating" value="1" />
+    <label for="star1" title="1 star">★</label>
+  </div>
         <label>YOUR FEEDBACk:</label><br></br>
         <textarea rows={5} placeholder='Enter your Message' required/><br></br>
 
